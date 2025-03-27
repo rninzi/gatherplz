883191834df7a0c4845025e7491d7f64a51b834b
mywhzy <cmj2393a@naver.com>
2025-03-27 18:34:18 +0900

    remove: GA 초기화 기능 제거

--- src/hooks/useGA.ts
+++ src/hooks/useGA.ts
@@ -5,22 +5,19 @@ import { gaApi } from '@/util/gaApi';
 
 export const useGAInitialize = () => {
   useEffect(() => {
-    const gaId = process.env.REACT_APP_GA_ID;
-    const gaDebug = process.env.REACT_APP_GA_DEBUG;
-
-    if (!gaId) {
-      throw new Error('Google Analytics ID not found');
-    }
-
-    if (gaDebug) {
-      console.log('Google Analytics Debug Mode');
-      ReactGA.initialize(gaId, {
-        gaOptions: { debug_mode: gaDebug === 'true' },
-      });
-      return;
-    }
-
-    ReactGA.initialize(gaId);
+    // const gaId = process.env.REACT_APP_GA_ID;
+    // const gaDebug = process.env.REACT_APP_GA_DEBUG;
+    // if (!gaId) {
+    //   throw new Error('Google Analytics ID not found');
+    // }
+    // if (gaDebug) {
+    //   console.log('Google Analytics Debug Mode');
+    //   ReactGA.initialize(gaId, {
+    //     gaOptions: { debug_mode: gaDebug === 'true' },
+    //   });
+    //   return;
+    // }
+    // ReactGA.initialize(gaId);
   }, []);
 };
 
